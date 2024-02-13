3dd2e1031e12139f57a7020fbe0d1abd01f658a3
GHOST-Aram <owinoojala@gmail.com>
2024-02-13 19:17:02 +0300

    feat: Handle POST requests with invalid input
         - Validate incoming data and respond with validation errors

--- src/urls/urls.ts
+++ src/urls/urls.ts
@@ -1,10 +1,15 @@
 import { Router } from "express"
 import { Controller } from "../controllers/controller"
+import { validateAllInput } from "./inputValidators"
+import { validator } from "../z-library/validation/validator"
 
 const router = Router()
 
 export const routesWrapper = (controller: Controller) =>{
 
     router.post('/:id', controller.respondWithMethodNotAllowed)
+    router.post('/', validateAllInput,
+        validator.handleValidationErrors
+    )
     return router
 }
