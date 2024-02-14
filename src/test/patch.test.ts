6f2268cd8ebbbffdbfc6945361b50b4496c6ba17
GHOST-Aram <owinoojala@gmail.com>
2024-02-14 09:05:19 +0300

    chore: Test patch input data validation
          - Write test that expects response with validation errors
          - Expect status response code 400
          - Send invalid input data

--- src/test/patch.test.ts
+++ src/test/patch.test.ts
@@ -1,7 +1,7 @@
 import { describe, test } from "@jest/globals";
 import request from "supertest"
 import { app } from "./config/test.config";
-import { patchData } from "./mocks/raw-data";
+import { patchData, badData } from "./mocks/raw-data";
 import { assert } from "../z-library/testing/response-assertion";
 
 describe('Gallery PATCH route', () => {
@@ -16,6 +16,17 @@ describe('Gallery PATCH route', () => {
     test('Responds with validation errors, status 400: Ivalid assetId', 
         async() => {
             const response = await request(app).patch('/gallery/64c9e4f2df7cc0tgd')
+
+            assert.respondsWithBadRequest(response)
+            assert.respondsWithValidationErrors(response)
+        }
+    )
+
+    
+    test('Responds with validation errors, status 400: Invalid input.', 
+        async() =>{
+            const response = await request(app).patch('/gallery/64c9e4f2df7cc072af2ac9e4')
+                .send(badData)
 
             assert.respondsWithBadRequest(response)
             assert.respondsWithValidationErrors(response)
